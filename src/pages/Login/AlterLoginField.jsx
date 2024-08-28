9e5687fba07d526fad24d46f806f46933ff05513
Thodoris Lazaris <lazaristheo@gmail.com>
2024-08-28 22:56:58 +0300

    updated css

--- src/pages/Login/AlterLoginField.jsx
+++ src/pages/Login/AlterLoginField.jsx
@@ -1,7 +1,7 @@
 function AlterLoginField({ icon, color_name, text, url_link }) {
   return (
-    <a className={"flex w-3/5 " + color_name} href={url_link}>
-      <img src={icon} alt="icon_img" />
+    <a className={"flex w-1/2 font-bold text-lg justify-center items-center mt-2 p-1 rounded " + color_name} href={url_link}>
+      <span className="p-1 flex-start">{icon}</span>
       {text}
     </a>
   );
--- src/pages/Login/LoginForm.jsx
+++ src/pages/Login/LoginForm.jsx
@@ -1,16 +1,20 @@
 import AlterLoginField from "./AlterLoginField";
 import login_signup from "../../assets/login_signup.avif";
 import { Button } from "@mui/material";
+import KeyIcon from '@mui/icons-material/Key';
+import Person2Icon from '@mui/icons-material/Person2';
+import FacebookIcon from '@mui/icons-material/Facebook';
+import GoogleIcon from '@mui/icons-material/Google';
 
 function LoginForm() {
   return (
     <div className="w-3/4 flex  mt-10 mb-10 m-auto rounded bg-gray-100 p-8 text-white">
-      <div className="w-1/2 bg-green-300">
-        <form id="login">
-          <h2 className="text-center">Join The Cooking Journey</h2>
-          <div className="flex justify-center p-2">
+      <div className="w-1/2 bg-[#01796F]">
+        <form id="login" className="flex flex-col">
+          <h2 className="text-center font-bold text-3xl">Join The Cooking Journey</h2>
+          <div className="flex justify-center items-center p-2">
             <label htmlFor="username">
-              <img src="" alt="username" />
+              <Person2Icon fontSize="large"/>
             </label>
             <input
               type="text"
@@ -19,14 +23,15 @@ function LoginForm() {
               className="form-input-design"
             />
           </div>
-          <div className="flex justify-center p-2">
-            <label>
-              <img src="" alt="password" />
+          <div className="flex justify-center items-center p-2">
+            <label htmlFor="password">
+              <KeyIcon fontSize="large"/>
             </label>
             <input
               type="password"
               placeholder="Password"
               className="form-input-design"
+              id="password"
             />
           </div>
           <div className="flex justify-center p-2">
@@ -39,16 +44,16 @@ function LoginForm() {
             </Button>
           </div>
 
-          <a href="" className="flex-1">
+          <a href="" className="flex-1 text-teal-200 hover:text-teal-400 ml-auto mr-2 font-semibold text-lg">
             Register now
           </a>
         </form>
-
-        <hr />
-
+       
+        <p className="text-2xl font-semibold text-center">·OR·</p>
+  
         <div className="flex flex-col  items-center">
-          <AlterLoginField text={"Google"} color_name={"bg-blue-400"} />
-          <AlterLoginField text={"FaceBook"} color_name={""} />
+          <AlterLoginField text={"Google"} color_name={"bg-[#fe4066] hover:bg-red-700"} icon={<GoogleIcon fontSize="large"/>} />
+          <AlterLoginField text={"Facebook"} color_name={"bg-[#1877F2] hover:bg-blue-700"} icon={<FacebookIcon fontSize="large"/>}/>
         </div>
       </div>
       <img src={login_signup} alt="side_image" className="w-1/2" />

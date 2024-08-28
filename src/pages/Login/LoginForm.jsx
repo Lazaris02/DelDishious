1448037f9bc83e985b32bcd7384c8aadd91661bb
Thodoris Lazaris <lazaristheo@gmail.com>
2024-08-28 23:48:52 +0300

    Update LoginForm.jsx

--- src/pages/Login/LoginForm.jsx
+++ src/pages/Login/LoginForm.jsx
@@ -8,10 +8,10 @@ import GoogleIcon from '@mui/icons-material/Google';
 
 function LoginForm() {
   return (
-    <div className="w-3/4 flex  mt-10 mb-10 m-auto rounded bg-gray-100 p-8 text-white">
-      <div className="w-1/2 bg-[#01796F]">
-        <form id="login" className="flex flex-col">
-          <h2 className="text-center font-bold text-3xl">Join The Cooking Journey</h2>
+    <div className="w-3/4 flex mt-10 mb-10 m-auto rounded bg-gray-100 p-8 text-white">
+      <div className="w-1/2 bg-[#01796F] flex flex-col space-y-4 flex-wrap-reverse">
+        <form id="login" className="flex flex-col space-y-4">
+          <h2 className="text-center font-bold text-3xl p-2">Join The Cooking Journey</h2>
           <div className="flex justify-center items-center p-2">
             <label htmlFor="username">
               <Person2Icon fontSize="large"/>
@@ -35,23 +35,24 @@ function LoginForm() {
             />
           </div>
           <div className="flex justify-center p-2">
-            <input type="checkbox" id="remember_me" />
-            <label htmlFor="remember_me">Remember me</label>
+            <input type="checkbox" id="remember_me" className="p-1 w-6 h-6 mr-1 rounded-full" />
+            <label htmlFor="remember_me" className="text-sm font-light mt-auto mb-auto">Remember me</label>
           </div>
           <div className="w-full flex justify-center">
-            <Button variant="contained" style={{ backgroundColor: "#FF6F61" }}>
-              Log in
+            <Button variant="contained" style={{ backgroundColor: "#FF7F50" , fontSize : "large" , color : "white", fontWeight : "bold" }}>
+              Login
             </Button>
           </div>
-
-          <a href="" className="flex-1 text-teal-200 hover:text-teal-400 ml-auto mr-2 font-semibold text-lg">
+          <span className="flex-1 p-2 ml-auto mr-2 font-semibold text-lg">
+          <a href="" className="hover:text-teal-400 text-teal-200">
             Register now
           </a>
+          </span>
         </form>
        
         <p className="text-2xl font-semibold text-center">·OR·</p>
   
-        <div className="flex flex-col  items-center">
+        <div className="flex flex-col  items-center p-2">
           <AlterLoginField text={"Google"} color_name={"bg-[#fe4066] hover:bg-red-700"} icon={<GoogleIcon fontSize="large"/>} />
           <AlterLoginField text={"Facebook"} color_name={"bg-[#1877F2] hover:bg-blue-700"} icon={<FacebookIcon fontSize="large"/>}/>
         </div>
